fd051e98d08c44de947f64c449d6fbebd5101fef
Naga Ajay <nagaajay@mac.mynetworksettings.com>
2026-02-03 15:19:30 -0500

    Instance creation

--- roboshop.sh
+++ roboshop.sh
@@ -5,24 +5,29 @@ AMI_ID="ami-0220d79f3f480ecf5"
 SUBNET_ID="subnet-0bb417478919bf408"
 
 for INSTANCE in "$@"; do
+
+  INSTANCE_ID=$(aws ec2 run-instances \
+    --image-id "$AMI_ID" \
+    --instance-type t3.micro \
+    --subnet-id "$SUBNET_ID" \
+    --security-group-ids "$SG_ID" \
+    --associate-public-ip-address \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
+    --query "Instances[0].InstanceId" \
+    --output text)
+
   if [[ "$INSTANCE" == "frontend" ]]; then
-    aws ec2 run-instances \
-      --image-id "$AMI_ID" \
-      --instance-type t3.micro \
-      --subnet-id "$SUBNET_ID" \
-      --security-group-ids "$SG_ID" \
-      --associate-public-ip-address \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
-      --query "Instances[].{InstanceId:InstanceId,PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress}" \
-      --output text
+    IP=$(aws ec2 describe-instances \
+      --instance-ids "$INSTANCE_ID" \
+      --query "Reservations[0].Instances[0].PublicIpAddress" \
+      --output text)
   else
-    aws ec2 run-instances \
-      --image-id "$AMI_ID" \
-      --instance-type t3.micro \
-      --subnet-id "$SUBNET_ID" \
-      --security-group-ids "$SG_ID" \
-      --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
-      --query "Instances[].{InstanceId:InstanceId,PrivateIP:PrivateIpAddress}" \
-      --output text
+    IP=$(aws ec2 describe-instances \
+      --instance-ids "$INSTANCE_ID" \
+      --query "Reservations[0].Instances[0].PrivateIpAddress" \
+      --output text)
   fi
+
+  echo "$INSTANCE  $IP"
+
 done
