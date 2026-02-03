01d0edacfa29f39c6c350dc0b48b7a29e887475f
Naga Ajay <nagaajay@mac.mynetworksettings.com>
2026-02-03 15:39:41 -0500

    Instance creation

--- roboshop.sh
+++ roboshop.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
+set -euo pipefail
 
 SG_ID="sg-0b1f7c3d25067bf9a"
 AMI_ID="ami-0220d79f3f480ecf5"
 SUBNET_ID="subnet-0bb417478919bf408"
 
-for INSTANCE in $@
- do
+for NAME in "$@"; do
+  ASSOC=""
+  Q="Reservations[0].Instances[0].PrivateIpAddress"
+  [[ "$NAME" == "frontend" ]] && ASSOC="--associate-public-ip-address" && Q="Reservations[0].Instances[0].PublicIpAddress"
 
-  INSTANCE_ID=$( aws ec2 run-instances \
+  ID=$(aws ec2 run-instances \
     --image-id "$AMI_ID" \
     --instance-type t3.micro \
+    --subnet-id "$SUBNET_ID" \
     --security-group-ids "$SG_ID" \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE}]" \
+    $ASSOC \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NAME}]" \
     --query "Instances[0].InstanceId" \
-    --output text )
+    --output text)
 
-  if [[ "$INSTANCE" == "frontend" ]]; then
-    IP=$(aws ec2 describe-instances \
-      --instance-ids "$INSTANCE_ID" \
-      --query "Reservations[0].Instances[0].PublicIpAddress" \
-      --output text)
-  else
-    IP=$(aws ec2 describe-instances \
-      --instance-ids "$INSTANCE_ID" \
-      --query "Reservations[0].Instances[0].PrivateIpAddress" \
-      --output text)
-  fi
+  aws ec2 wait instance-running --instance-ids "$ID"
+  IP=$(aws ec2 describe-instances --instance-ids "$ID" --query "$Q" --output text)
 
-  echo "$INSTANCE, Ip adress: $IP"
-
+  echo "$NAME  $ID  $IP"
 done
+
