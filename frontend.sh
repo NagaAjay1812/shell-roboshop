1748aae17403af15da21f113a88cdc4357b4fa7c
Naga Ajay <nagaajay@Nagas-Mac-mini.local>
2026-02-03 22:34:18 -0500

    Frontend  application  Installation

--- frontend.sh
+++ frontend.sh
@@ -3,6 +3,7 @@ USERID=$(id -u)
 
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="/var/log/shell-roboshop/$0.log"
+SCRIPT_DIR=$PWD
 
 R="\e[31m"
 G="\e[32m"
@@ -26,6 +27,37 @@ VALIDATE(){
     fi
 }
 
+dnf module list nginx &>> $LOGS_FILE
+VALIDATE $? "Module list of nginx"
 
-dnf module list nginx
-VALIDATE $? "change directory to app"
+
+dnf module enable nginx:1.24 -y &>> $LOGS_FILE
+VALIDATE $? "Enable nginx:24 version"
+
+dnf list installed nginx &>> $LOGS_FILE
+if [ $? -ne 0 ]; then
+    echo "nginx is  not installed, installing now" | tee -a $LOGS_FILE
+    dnf install ginx -y &>> $LOGS_FILE
+    VALIDATE $? "Installing nginx"
+else
+    echo -e "nginx is already installed, $Y SKIPPING $N" | tee -a $LOGS_FILE
+fi
+
+rm -rf /usr/share/nginx/html/* 
+VALIDATE $? "Remove the default html content"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+VALIDATE $? "Downloding code from s3 location"
+
+cd /usr/share/nginx/html &>> $LOGS_FILE
+VALIDATE $? "change directory to html"
+
+unzip /tmp/frontend.zip &>> $LOGS_FILE 
+VALIDATE $? "unzip the code"
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "copying the nginx conf and update DNS records"
+
+systemctl restart nginx 
+VALIDATE $? "Restart nginx"
+
